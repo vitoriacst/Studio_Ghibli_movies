a5c29ff82706be0b24818c15da63ecc37ed644e2
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-09-26 21:21:51 -0300

    refactor: :recycle: Refatorando o useApi

--- app/src/hooks/useApi.js
+++ app/src/hooks/useApi.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
 
-const UseApi = () => {
+const UseApi = (url) => {
   const [moviesData, setMoviesData] = useState([]);
-  useEffect((url) => {
+  useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setMoviesData(data);
       });
-  });
+  }, []);
+
   return { moviesData };
 };
 
